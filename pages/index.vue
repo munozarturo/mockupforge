9f73d6be0caa3a1cb648c16a30b10b9c4848bc73
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-26 14:16:19 -0400

    build: add nuxt/image and init style

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Mockupforge</div>
+    <div class="w-full h-screen flex flex-col items-center justify-center">
+        <NuxtImg class="w-1/5 h-1/5" src="/brand/logo_wide.svg" />
+        <h1 class="text-3xl font-bold">under construction...</h1>
+    </div>
 </template>
 
 <script lang="ts" setup></script>
